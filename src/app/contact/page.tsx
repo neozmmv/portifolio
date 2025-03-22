0885d74a04c3b0ad8f4e3fde46486171fb166e69
Enzo Gutiérrez Pereira <enzogp2005@gmail.com>
2025-03-22 15:22:10 -0300

    Página de contato ajustada para mobile

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,9 +4,9 @@ export default function Contact() {
   return (
     <>
       <Nav />
-      <div className="w-full h-screen bg-slate-950 flex  justify-between ">
+      <div className="w-full h-screen bg-slate-950 flex justify-between ">
         <div className="w-full bg-slate-900 m-2 p-2 rounded-xl flex justify-center align-center">
-          <div className=" bg-slate-950 rounded-4xl w-[500px] h-[280px] mt-40 flex-col justify-center ">
+          <div className=" bg-slate-950 sm:mr-0 rounded-4xl w-auto h-68 p-6 sm:w-[500px] sm:h-[280px] mt-40 flex-col justify-center ">
             <h1 className="font-bold text-white text-3xl pt-6 font-regular text-center">
               Entre em contato por:
             </h1>
